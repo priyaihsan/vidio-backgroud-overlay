7c4ef7222b46d6d7026018cd0e4b844857c218d5
priya ihsan <priyaihsan771@gmail.com>
2021-12-12 16:55:49 +0700

    return to normal

--- index.php
+++ index.php
@@ -1,9 +1,1 @@
-<?php 
-
-$getdata = $_POST['add-vidio'];
-$index = 'localstorage.html';
-if($getdata != null){
-    $index = 'second.html';
-}
-
-include_once($index); ?>
+<?php include_once('second.html'); ?>
